5d4632c7956c82055a117dbb4dd668dce7d414d6
Geidel Guerra <geidelguerra@gmail.com>
2018-07-26 11:47:11 -0400

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="container h-screen flex justify-center items-center">
+    <div>
+      <h1 class="text-2xlg text-center text-green-light">
+        Geidel Guerra
+      </h1>
+      <h2 class="text-center text-grey-dark">
+        My personal web site
+      </h2>
+    </div>
+  </section>
+</template>
