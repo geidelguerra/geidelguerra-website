b828861ada8a06fbc4e847e5203ab8d5ca72a3c7
Geidel Guerra <geidelguerra@gmail.com>
2020-03-14 18:27:03 -0400

    Update keywords.

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Software Developer' },
-      { hid: 'keywords', name: 'keywords', content: 'software,fullstack,web,vuejs,laravel,node,mysql,nuxt' },
+      { hid: 'keywords', name: 'keywords', content: 'software,fullstack,web,vuejs,laravel,node,mysql,nuxt,threejs,ansible' },
       // Open graph
       { hid: 'og:title', property: 'og:title', content: 'Geidel Guerra' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
