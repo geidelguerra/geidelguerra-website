5963ece5e884427c14e24ee2463f97d1721d22f8
Geidel Guerra <geidelguerra@protonmail.com>
2021-08-31 21:10:12 -0400

    Add profile picture, twitter network and some styling

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,25 @@
 <template>
-  <section class="container mx-auto flex justify-center items-center h-full">
-    <div>
-      <h1 class="text-center font-thin text-4xl mb-4">
+  <section class="container flex items-center justify-center h-full mx-auto">
+    <div class="flex flex-col items-center justify-center p-6 mx-2 bg-gray-900 rounded-lg">
+      <img src="/images/geidel_profile.jpg" alt="Geidel Guerra Picture" class="mb-6 overflow-hidden rounded-full w-28 h-28">
+      <h1 class="mb-4 text-4xl font-thin text-center">
         Geidel Guerra
       </h1>
-      <h2 class="text-center font-mono uppercase text-sm font-bold">
-        Software Developer - Laravel, Vuejs, Unity, Ansible.
+      <h2 class="font-mono text-sm font-bold text-center uppercase">
+        Software Developer - Laravel, Vuejs, Ansible.
       </h2>
-      <div class="text-center p-2">
-        <a
-          href="https://linkedin.com/in/geidelguerra"
-          target="_blank"
-          class="text-green-500"
-        >LinkedIn</a>
-        <a
-          href="https://github.com/geidelguerra"
-          target="_blank"
-          class="text-green-500"
-        >GitHub</a>
+      <div class="p-2 space-x-1 text-center">
+        <template v-for="network in networks">
+          <a
+            :key="network.url"
+            :href="network.url"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="text-green-500 hover:text-green-300"
+          >
+            {{ network.label }}
+          </a>
+        </template>
       </div>
     </div>
   </section>
@@ -25,6 +27,14 @@
 
 <script>
 export default {
-  //
+  data() {
+    return {
+      networks: [
+        { label: 'LinkedIn', url: 'https://linkedin.com/in/geidelguerra' },
+        { label: 'GitHub', url: 'https://github.com/geidelguerra' },
+        { label: 'Twitter', url: 'https://twitter.com/geidelguerra' }
+      ]
+    }
+  }
 }
 </script>
