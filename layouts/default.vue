eb23b8c549ebf93d0cb651ec50222e35df43c172
Geidel Guerra <geidelguerra@protonmail.com>
2022-05-04 09:01:43 -0400

    Add netlify badge

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,20 @@
   <div class="flex flex-col min-h-screen font-sans text-white bg-black">
     <nuxt class="flex-1" />
 
-    <footer>
+    <footer class="flex flex-col items-center justify-center mb-4">
       <div class="py-2 text-xs font-bold text-center text-green-500 uppercase">
         Copyright {{ new Date().getFullYear() }} &copy; Geidel Guerra
       </div>
+      <a
+        href="https://www.netlify.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://www.netlify.com/v3/img/components/netlify-color-accent.svg"
+          alt="Deploys by Netlify"
+        />
+      </a>
     </footer>
   </div>
 </template>
