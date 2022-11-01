32389a96f41d9ac823d66dcca3cd6dd561f4e796
Geidel Guerra <geidelguerra@protonmail.com>
2022-10-31 20:20:22 -0400

    WIP

---
+++ generate_cv.js
@@ -0,0 +1,76 @@
+const data = require('./data');
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+
+const doc = new PDFDocument();
+
+doc.pipe(fs.createWriteStream('./static/files/geidelguerra_cv_en.pdf'));
+
+doc.image('static/images/geidel_profile.jpg', 70, 20, { fit: [70, 70]})
+doc.text('Geidel Guerra', 150, 30)
+
+doc.text('', 70, 40)
+doc.moveDown(5)
+
+doc
+  .font('assets/fonts/pdf/FiraSans-Black.ttf')
+  .fontSize(18)
+  .text('Who am I?')
+  .moveDown(0.5)
+
+doc
+  .font('assets/fonts/pdf/FiraSans-Regular.ttf')
+  .fontSize(12)
+  .text(data.about, { align: 'justify'})
+
+doc.moveDown()
+doc.font('assets/fonts/pdf/FiraSans-Black.ttf').fontSize(18).text('Skills').moveDown(0.5)
+
+data.skills.forEach((item) => {
+  doc
+    .font('assets/fonts/pdf/FiraSans-Regular.ttf')
+    .fontSize(12)
+    .text(`${item.label} - ${item.score}`)
+})
+
+doc.moveDown()
+doc.font('assets/fonts/pdf/FiraSans-Black.ttf').fontSize(18).text('Languages').moveDown(0.5)
+
+data.languages.forEach((item) => {
+  doc
+    .font('assets/fonts/pdf/FiraSans-Regular.ttf')
+    .fontSize(12)
+    .text(`${item.label} - ${item.score}`, { link: item.url })
+})
+
+doc.moveDown()
+doc.font('assets/fonts/pdf/FiraSans-Black.ttf').fontSize(18).text('Toolkit').moveDown(0.5)
+
+data.toolkit.forEach((item) => {
+  doc
+    .font('assets/fonts/pdf/FiraSans-Regular.ttf')
+    .fontSize(12)
+    .text(`${item.category} - ${item.name}`)
+})
+
+doc.moveDown()
+doc.font('assets/fonts/pdf/FiraSans-Black.ttf').fontSize(18).text('Projects').moveDown(0.5)
+
+data.projects.forEach((item) => {
+  doc
+    .font('assets/fonts/pdf/FiraSans-Bold.ttf')
+    .fontSize(12)
+    .text(item.name, { link: item.url })
+    .font('assets/fonts/pdf/FiraSans-Regular.ttf')
+    .fontSize(10)
+    .fillColor('#888')
+    .text(`${item.startDate} - ${item.endDate}`)
+    .fillColor('black')
+    .moveDown()
+    .fontSize(12)
+    .font('assets/fonts/pdf/FiraSans-Regular.ttf')
+    .text(item.description)
+    .moveDown()
+})
+
+doc.save().end();
--- pages/index.vue
+++ pages/index.vue
@@ -2,36 +2,7 @@
   <div class="mx-auto max-w-screen-md w-full flex flex-col space-y-8 px-4 md:px-0">
     <section>
       <h2 class="font-black text-3xl tracking-tight mb-4">Who am I?</h2>
-      <div class="prose prose-invert">
-        <p>
-          I'm a self taught "Fullstack Developer" specialized in Laravel, VueJs and TailwindCSS. Hoping to make some
-          games in the future.
-        </p>
-        <p>
-          I've been learning to code for myself since I was 16. I started with Macromedia Flash 8 offline help. Then I
-          jumped to C++, C#, Java, Python and eventually landed with PHP and JavaScript.
-        </p>
-        <p>
-          I have experience developing SaaS applications, REST APIs, Third-Party integrations (Facebook, Twitter,
-          Google, LinkedIn, YouTube, Stripe) and basic websites using NuxtJs and Vue.
-        </p>
-        <p>
-          I have some experience with AWS and DevOps. I use Digital Ocean for my personal stuff.
-        </p>
-        <p>
-          I have basic knowledge of Git. I use GitHub for all my personal and company projects.
-        </p>
-        <p>
-          I've been using Ubuntu Desktop for about 6 years. I use ZSH, Kitty and VSCode.
-        </p>
-        <p>
-          I know about Jira, Trello, ClickUp and other project management tools. Also know a little about SCRUM
-          methodology and have been using it for a while.
-        </p>
-        <p>
-          Proficient in English, native Spanish speaker.
-        </p>
-      </div>
+      <div v-html="about.replace(/\n/g, '<br>')" class="prose prose-invert"/>
     </section>
     <section>
       <h2 class="font-black text-3xl tracking-tight mb-4">Skills</h2>
@@ -112,93 +83,10 @@
 </template>
 
 <script>
+import data from '~/data.js'
 export default {
   data() {
-    return {
-      networks: [
-        { label: 'LinkedIn', url: 'https://linkedin.com/in/geidelguerra' },
-        { label: 'GitHub', url: 'https://github.com/geidelguerra' },
-        { label: 'Twitter', url: 'https://twitter.com/geidelguerra' }
-      ],
-      skills: [
-        { label: 'Laravel', score: 5, url: 'https://laravel.com' },
-        { label: 'VueJs', score: 4, url: 'https://vuejs.org' },
-        { label: 'InertiaJs', score: 4, url: 'https://inertiajs.com' },
-        { label: 'DevOps', score: 4, url: 'https://www.martinfowler.com/bliki/DevOpsCulture.html' },
-        { label: 'Ansible', score: 2.5, url: 'https://www.ansible.com' },
-        { label: 'Git', score: 3.5, url: 'https://git-scm.com' },
-        { label: 'MySQL', score: 2.5, url: 'https://www.mysql.com' },
-        { label: 'NodeJs', score: 3, url: 'https://nodejs.org' },
-        { label: 'AWS', score: 3, url: 'https://console.aws.amazon.com/' },
-        { label: 'Laravel Vapor', score: 3.5, url: 'https://vapor.laravel.com' },
-        { label: 'Laravel Forge', score: 3.5, url: 'https://forge.laravel.com' },
-        { label: 'Laravel Envoyer', score: 3.5, url: 'https://envoyer.io' },
-      ].sort((a, b) => {
-        if (a.score > b.score) {
-          return -1
-        }
-
-        if (a.score < b.score) {
-          return 1
-        }
-
-        return 0
-      }),
-      toolkit: [
-        { category: 'OS', name: 'Ubuntu 22.04' },
-        { category: 'Editor', name: 'VSCode' },
-        { category: 'Terminal', name: 'Kitty, Tmux' },
-        { category: 'Shell', name: 'ZSH' },
-      ],
-      languages: [
-        { label: 'Spanish', score: 'Native' },
-        { label: 'English', score: 'Proficient (C2)', url: 'https://www.efset.org/cert/TNPV3e' },
-      ],
-      projects: [
-        {
-          name: 'Tenza Website',
-          url: 'https://tenza.us',
-          startDate: '2021-11',
-          endDate: 'Present',
-          description: 'Official website for Tenza Studio. Tech used: NuxtJs + TailwindCSS + ThreeJs + AnimeJs and Lottie. Hosted on Netlify'
-        },
-        {
-          name: 'Alberto Hernandez Reyes Personal Website',
-          url: 'https://albertohreyes.com/',
-          startDate: '2018-06',
-          endDate: '2021-12',
-          description: 'Official website of the Cuban artist Alberto Hernandez. Tech used: NuxtJs + TailwindCSS. Hosted on Netlify'
-        },
-        {
-          name: 'La Tinta Magazine Website',
-          url: 'https://web.archive.org/web/20210119105411/https://www.latintamagazine.com/',
-          startDate: '2018-03',
-          endDate: '2021-01',
-          description: 'Official website for the first Cuban magazine about body art. Tech used: NuxtJs + TailwindCSS. Hosted on Netlify'
-        },
-        {
-          name: 'PlaceArt Website',
-          url: 'https://www.placeart.app/',
-          startDate: '2019-02',
-          endDate: '2020-12',
-          description: '"PlaceArt aims to lower Art access barriers through Augmented and Virtual Reality experiences that allow to visualize and customize fine art prints from the best of Public Domain Museum Art pieces around the world". Tech used: NuxtJs + Laravel + ThreeJs + AFrame'
-        },
-        {
-          name: 'Fábrica de Arte Cubano Website',
-          url: 'https://web.archive.org/web/20180618093513/https://fac.cu/',
-          startDate: '2015-01',
-          endDate: '2018-06',
-          description: 'Official website for the multicultural site Fábrica de Arte Cubano. Tech used: Laravel 5 + Bootstrap + jQuery'
-        },
-        {
-          name: 'Gráfica Interactiva II (with Serones Art Group)',
-          url: 'https://www.youtube.com/watch?v=4kQNjOp4UJ8',
-          startDate: '2017-01',
-          endDate: '2017-03',
-          description: 'This was a collaboration with a cuban art group called Serones. I was tasked with the coding of the 3D navigation system, event triggers and illumination. Tech used: Unity3D'
-        },
-      ]
-    }
+    return data
   }
 }
 </script>
