34383e501f052e2233d17fad7179b034090b6c9a
Geidel Guerra <geidelguerra@protonmail.com>
2022-11-02 09:57:04 -0400

    WIP

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,7 +11,10 @@
         <ul class="flex space-x-2">
           <template v-for="navItem in navMenu">
             <li :key="navItem.url">
-              <nuxt-link :to="navItem.url" exact-active-class="bg-slate-600" class="text-blue-300 px-2 py-1 hover:bg-slate-600 rounded transition-all whitespace-nowrap">{{ navItem.text }}</nuxt-link>
+              <nuxt-link :to="navItem.url" exact-active-class="bg-slate-600"
+                class="text-blue-300 px-2 py-1 hover:bg-slate-600 rounded transition-all whitespace-nowrap">{{
+                    navItem.text
+                }}</nuxt-link>
             </li>
           </template>
         </ul>
@@ -22,25 +25,35 @@
       <nuxt />
     </main>
 
-    <footer class="flex flex-col items-center space-y-4 p-4 mx-auto max-w-3xl md:px-0 sm:flex-row sm:justify-between sm:space-y-0 sm:w-full">
-      <div>
-        <a target="_blank" class="text-blue-300 px-2 py-1 hover:bg-slate-600 rounded transition-all" href="/files/geidelguerra_cv_en.pdf">Download CV</a>
-      </div>
-      <div class="flex flex-col">
-        <h3 class="text-xs text-green-300 mb-1 text-center sm:text-left">Where to find me?</h3>
-        <ul class="flex space-x-2">
-          <template v-for="link in networks">
-            <li :key="link.url">
-              <a class="hover:underline text-blue-300" :href="link.url" target="_blank" rel="noopener noreferrer">{{ link.text }}</a>
-            </li>
-          </template>
-        </ul>
-      </div>
-      <div class="flex flex-col">
-        <h3 class="text-xs mb-1 text-center sm:text-right"><span class="text-green-300">Deployed using</span> <a class="hover:underline text-blue-300" target="_blank" rel="noopener noreferrer" href="https://netlify.com/">Netlify</a></h3>
-        <div class="text-sm text-white">
-          Copyright {{ new Date().getFullYear() }} &copy; Geidel Guerra
+    <footer class="p-4 mx-auto max-w-3xl md:px-0 w-full flex flex-col space-y-4">
+      <div class="flex flex-col items-center space-y-4 sm:flex-row sm:justify-between sm:space-y-0 sm:w-full">
+        <div>
+          <a target="_blank" class="text-blue-300 px-2 py-1 bg-slate-700 hover:bg-slate-600 rounded transition-all"
+            href="/files/geidelguerra_cv_en.pdf">Download CV</a>
         </div>
+        <div class="flex flex-col">
+          <h3 class="text-xs text-green-300 mb-1 text-center sm:text-left">Where to find me?</h3>
+          <ul class="flex space-x-2">
+            <template v-for="link in networks">
+              <li :key="link.url">
+                <a class="hover:underline text-blue-300" :href="link.url" target="_blank" rel="noopener noreferrer">{{
+                    link.text
+                }}</a>
+              </li>
+            </template>
+          </ul>
+        </div>
+        <div class="flex flex-col">
+          <h3 class="text-xs mb-1 text-center sm:text-right"><span class="text-green-300">Deployed using</span> <a
+              class="hover:underline text-blue-300" target="_blank" rel="noopener noreferrer"
+              href="https://netlify.com/">Netlify</a></h3>
+          <div class="text-sm text-white">
+            Copyright {{ new Date().getFullYear() }} &copy; Geidel Guerra
+          </div>
+        </div>
+      </div>
+      <div class="text-center">
+        <a class="text-sm text-slate-600 hover:underline font-mono" href="https://github.com/geidelguerra/geidelguerra-website" target="_blank" rel="noopener noreferrer">The source code</a>
       </div>
     </footer>
   </div>
