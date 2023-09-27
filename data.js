932496c1a35ca4e35e75372082e065c6545a949b
Geidel Guerra <cayohuesodev@pm.me>
2023-09-27 11:27:28 -0400

    wip

--- data.js
+++ data.js
@@ -1,23 +1,19 @@
 module.exports = {
-  about: `I'm a self taught "Software Developer" specialized in PHP and JavaScript. Hoping to make some games in the future.
+  about: `I'm a self taught "Software Developer" specialized in PHP, JavaScript and Python. Hoping to make at least 1 minigame in the future.
 
-I've been learning to code for myself since I was 16. I started with Macromedia Flash 8 offline help. Then I jumped to C++, C#, Java, Python and eventually landed with PHP and JavaScript.
-
-I have experience developing SaaS applications, REST APIs, Third-Party integrations (Facebook, Twitter, Google, LinkedIn, YouTube, Stripe) and basic websites using NuxtJs and Vue.
-
-I have some experience with AWS and DevOps. I use Digital Ocean for my personal stuff.
-
-I have basic knowledge of Git. I use GitHub for all my personal and company projects.
-
-I've been using Ubuntu Desktop for about 6 years. I use ZSH, Kitty and VSCode.
-
-I know about Jira, Trello, ClickUp and other project management tools. Also know a little about SCRUM methodology and have been using it for a while.
-
-Unit/Feature tests when possible.
-
-Proficient in English, native Spanish speaker.
-
-Learning Python and AI.
+  I've been learning to code for myself since I was 16. I started with Macromedia Flash 8 offline help. Then I jumped to C++, C#, Java, Python and eventually landed with PHP and JavaScript where I spent most of my time until recently when I started to work with Python (for real) to create tools and web applications and I love it.
+  
+  I have experience developing SaaS applications, REST APIs, Third-Party integrations (Facebook, Twitter, Google, LinkedIn, YouTube, Stripe) and basic websites using NuxtJs and Vue.
+  
+  I have some experience with AWS and DevOps. I use Digital Ocean for my personal stuff.
+  
+  I have basic knowledge of Git. I use GitHub for all my personal and company projects.
+  
+  I've been using Ubuntu Desktop for about 6 years. I use ZSH, Kitty and VSCode.
+  
+  I know about Jira, Trello, ClickUp and other project management tools. Also know a little about SCRUM methodology and have been using it for a while.
+  
+  Proficient in English, native Spanish speaker.
   `,
   networks: [
     { label: 'LinkedIn', url: 'https://linkedin.com/in/geidelguerra' },
