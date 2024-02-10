f530b3aa58bc4f1a345cb042262f24a38786eb8e
Geidel Guerra <geidel@acer-win>
2024-02-10 15:28:13 -0500

    wip

--- data.js
+++ data.js
@@ -1,19 +1,15 @@
 module.exports = {
-  about: `I'm a self taught "Software Developer" specialized in PHP, JavaScript and Python. Hoping to make at least 1 minigame in the future.
+  about: `I'm a self taught developer specialized in web and cli tools. Hoping to make some games in the future.
 
-  I've been learning to code for myself since I was 16. I started with Macromedia Flash 8 offline help. Then I jumped to C++, C#, Java, Python and eventually landed with PHP and JavaScript where I spent most of my time until recently when I started to work with Python (for real) to create tools and web applications and I love it.
-  
-  I have experience developing SaaS applications, REST APIs, Third-Party integrations (Facebook, Twitter, Google, LinkedIn, YouTube, Stripe) and basic websites using NuxtJs and Vue.
-  
-  I have some experience with AWS and DevOps. I use Digital Ocean for my personal stuff.
-  
-  I have basic knowledge of Git. I use GitHub for all my personal and company projects.
-  
-  I've been using Ubuntu Desktop for about 6 years. I use ZSH, Kitty and VSCode.
-  
-  I know about Jira, Trello, ClickUp and other project management tools. Also know a little about SCRUM methodology and have been using it for a while.
-  
-  Proficient in English, native Spanish speaker.
+I've been learning to code for myself since I was 16. I started with Macromedia Flash 8 offline help. Then I jumped to C++, C#, Java, Python and eventually landed with PHP and JavaScript. Now mostly working on cli tools using Python.
+
+I prefer working on cli tools and third party integrations on the backend to improve workflows, add new features to existing systems, create new systems. I like the challenge of learning a new language or tooling to solve a problem.
+
+I have some experience with AWS and DevOps. I use Digital Ocean for my personal stuff.
+
+I've been using Ubuntu Desktop for about 6 years. I use ZSH, Kitty and VSCode.
+
+Proficient in English, native Spanish speaker.
   `,
   networks: [
     { label: 'LinkedIn', url: 'https://linkedin.com/in/geidelguerra' },
