3860b1afc465d88121f7a3255f54cb24f657953a
Geidel Guerra <github.6870z@passmail.net>
2024-05-01 06:29:44 -0400

    wip

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 module.exports = {
   about: `I'm a self taught developer specialized in web and cli tools. Hoping to make some games in the future.
 
-I've been learning to code for myself since I was 16. I started with Macromedia Flash 8 offline help. Then I jumped to C++, C#, Java, Python and eventually landed with PHP and JavaScript. Now mostly working on cli tools using Python.
+I've been learning to code for myself since I was 16. I started with Macromedia Flash 8 offline help. Then I jumped to C++, C#, Java, Python and eventually landed with PHP and JavaScript. Now mostly working on cli tools using Python and learning C by making games with Raylib.
 
 I prefer working on cli tools and third party integrations on the backend to improve workflows, add new features to existing systems, create new systems. I like the challenge of learning a new language or tooling to solve a problem.
 
@@ -19,6 +19,7 @@ Proficient in English, native Spanish speaker.
     { label: 'Python', url: 'https://www.python.org/' },
     { label: 'PHP', url: 'https://php.net' },
     { label: 'JavaScript', url: 'https://php.net' },
+    { label: 'C', url: 'https://en.wikipedia.org/wiki/C_(programming_language)' },
     { label: 'MongoDB', url: 'https://www.mongodb.com/' },
     { label: 'MySQL', url: 'https://www.mysql.com' },
   ],
