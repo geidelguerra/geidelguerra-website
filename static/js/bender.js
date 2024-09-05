0fe20e41ef0c4725a65b098018d8203f26ca4100
Geidel Guerra <github.6870z@passmail.net>
2024-09-05 19:36:03 -0400

    wip

--- static/js/bender.js
+++ static/js/bender.js
@@ -7,25 +7,26 @@
     if (!rightEye) return
 
     const leftEyeCenter = {x: 36, y: 50}
-    const leftEyeCenterAbsolute = {
-        x: bender.offsetLeft - (bender.offsetWidth / 2) + leftEyeCenter.x + (leftEye.offsetWidth / 2),
-        y: bender.offsetTop + bender.offsetHeight + 13 + leftEyeCenter.y
-    }
     const rightEyeCenter = {x: 58, y: 50}
-    const rightEyeCenterAbsolute = {
-        x: bender.offsetLeft - (bender.offsetWidth / 2) + rightEyeCenter.x + (rightEye.offsetWidth / 2),
-        y: bender.offsetTop + bender.offsetHeight + 13 + rightEyeCenter.y
-    }
     const eyeRadius = 5
     let mousePosition = null
 
     function updateEyes() {
         if (mousePosition) {
+            const leftEyeCenterAbsolute = {
+                x: bender.offsetLeft - (bender.offsetWidth / 2) + leftEyeCenter.x + (leftEye.offsetWidth / 2),
+                y: bender.offsetTop + bender.offsetHeight + 13 + leftEyeCenter.y
+            }
             let leftAngle = -Math.atan2(mousePosition.y - leftEyeCenterAbsolute.y, mousePosition.x - leftEyeCenterAbsolute.x) * -1;
             let leftX = leftEyeCenter.x + Math.cos(leftAngle) * eyeRadius;
             let leftY = leftEyeCenter.y + Math.sin(leftAngle) * eyeRadius;
             leftEye.style.left = `${leftX}px`
             leftEye.style.top = `${leftY}px`
+
+            const rightEyeCenterAbsolute = {
+                x: bender.offsetLeft - (bender.offsetWidth / 2) + rightEyeCenter.x + (rightEye.offsetWidth / 2),
+                y: bender.offsetTop + bender.offsetHeight + 13 + rightEyeCenter.y
+            }
 
             let rightAngle = -Math.atan2(mousePosition.y - rightEyeCenterAbsolute.y, mousePosition.x - rightEyeCenterAbsolute.x) * -1;
             let rightX = rightEyeCenter.x + Math.cos(rightAngle) * eyeRadius;
